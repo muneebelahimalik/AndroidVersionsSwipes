ff5041f91cd3fbffd1370c808806c8e2543bdc19
Muneeb Elahi Malik <mm17889@uga.edu>
2024-10-23 11:01:27 -0400

    Initial commit

---
+++ app/src/main/java/edu/uga/cs/androidversionsswipes/MainActivity.java
@@ -0,0 +1,33 @@
+package edu.uga.cs.androidversionsswipes;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ViewPager2 pager = findViewById( R.id.viewpager );
+        AndroidVersionsPagerAdapter avpAdapter = new
+                AndroidVersionsPagerAdapter(
+                getSupportFragmentManager(), getLifecycle() );
+        pager.setOrientation(
+                ViewPager2.ORIENTATION_HORIZONTAL );
+        pager.setAdapter( avpAdapter );
+    }
+}
